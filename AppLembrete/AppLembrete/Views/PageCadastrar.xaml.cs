b4c3d8ee29347201183bdf2258a9721478421f3b
AprendizadoConta <joaossilvapereira@gmail.com>
2024-02-20 21:43:02 -0300

    Atualização Final

--- AppLembrete/AppLembrete/Views/PageCadastrar.xaml.cs
+++ AppLembrete/AppLembrete/Views/PageCadastrar.xaml.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using AppLembrete.Models;//adicionei
+using AppLembrete.Services;//adicionei
+
 namespace AppLembrete.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +18,66 @@ namespace AppLembrete.Views
         public PageCadastrar()
         {
             InitializeComponent();
+        }
+        public PageCadastrar(ModelNotas nota)
+        {
+            InitializeComponent();
+            btSalvar.Text = "Alterar";
+            txtCodigo.IsVisible = true;
+            btExcluir.IsVisible = true;
+            txtCodigo.Text = nota.Id.ToString();
+            txtTitulo.Text = nota.Titulo;
+            txtDados.Text = nota.Dados;
+            swFavorito.IsToggled = nota.Favorito;
+        }
+
+        private void btSalvar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                ModelNotas notas = new ModelNotas();
+                notas.Titulo = txtTitulo.Text;
+                notas.Dados = txtDados.Text;
+                notas.Favorito = swFavorito.IsToggled;//Istoggled é se o botão está ativado ou não
+                ServiceDBNotas dBNotas = new ServiceDBNotas(App.DbPath);
+                if (btSalvar.Text == "Inserir")
+                {
+                    dBNotas.Inserir(notas);
+                    DisplayAlert("Resultado", dBNotas.StatusMessage, "OK");
+                }
+                else
+                {
+                    notas.Id = Convert.ToInt32(txtCodigo.Text);
+                    dBNotas.Alterar(notas);
+                    DisplayAlert("Nota alterada com sucesso!",dBNotas.StatusMessage,"OK");
+                }
+                MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+                p.Detail = new NavigationPage(new PageHome());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private async void btExcluir_Clicked(object sender, EventArgs e)
+        {
+            var resp = await DisplayAlert("Excluir Nota", "Deseja EXCLUIR a nota selecionada?", "Sim", "Não"); //como eu quero uma resposta do DisplayAlert, eu preciso colocar esse await /o SIM é true e o NÃO é FALSE
+            if (resp==true)
+            {
+                ServiceDBNotas dBNotas = new ServiceDBNotas(App.DbPath);
+                int id = Convert.ToInt32(txtCodigo.Text);
+                dBNotas.Excluir(id);
+                DisplayAlert("Nota excluída com sucesso", dBNotas.StatusMessage, "OK");
+                MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+                p.Detail = new NavigationPage(new PageHome());
+            }
+        }
+
+        private void btCancelar_Clicked(object sender, EventArgs e)
+        {
+            MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+            p.Detail = new NavigationPage(new PageHome());
         }
     }
 }
--- AppLembrete/AppLembrete/Views/PageListar.xaml.cs
+++ AppLembrete/AppLembrete/Views/PageListar.xaml.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using AppLembrete.Models;//adicionei
+using AppLembrete.Services;//adicionei
+
 namespace AppLembrete.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +18,40 @@ namespace AppLembrete.Views
         public PageListar()
         {
             InitializeComponent();
+            AtualizaLista();
+        }
+
+        public void AtualizaLista()
+        {
+            String titulo = "";
+            if (txtNota.Text != null) titulo = txtNota.Text;
+            ServiceDBNotas dBNotas = new ServiceDBNotas(App.DbPath);
+            if (swFavorito.IsToggled)
+            {
+                ListaNotas.ItemsSource = dBNotas.Localizar(titulo, true);
+            }
+            else
+            {
+                ListaNotas.ItemsSource = dBNotas.Localizar(titulo);
+            }
+        }
+
+        private void swFavorito_Toggled(object sender, ToggledEventArgs e)
+        {
+            AtualizaLista();
+        }
+
+        private void btLocalizar_Clicked(object sender, EventArgs e)
+        {
+            AtualizaLista();
+        }
+
+        private void ListaNotas_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ModelNotas notas = (ModelNotas)ListaNotas.SelectedItem;
+            MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+            p.Detail = new NavigationPage(new PageCadastrar(notas));
+
         }
     }
 }
